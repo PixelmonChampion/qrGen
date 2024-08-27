5fb05322cdfa61d6547021ccae9bae5102c25e8b
Ozzy <32597237+PixelmonChampion@users.noreply.github.com>
2024-08-27 15:50:16 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -2,16 +2,6 @@ import qrcode
 
 url = input("Enter the URL: ")
 
-qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=10,
-    border=4,
-)
-
-qr.add_data(url)
-qr.make(fit=True)
-
-img = qr.make_image(fill_color='black', back_color='white')
+img = qrcode.make(url)
 
 img.save("qr.png")
